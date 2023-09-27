e1ef33977d8e70c3c1184cea93732e2aadc418bc
willianfhs <willfhschneider@gmail.com>
2023-09-26 23:33:18 -0300

    added delete function and remove prints

--- setdata.py
+++ setdata.py
@@ -3,32 +3,48 @@
 # justificativa para a escolha da estrutura de dados, e a complexidade de tempo e espaço esperada para cada uma das operações.
 
 class HashTable(object):
-     
     def __init__(self,size):
         self.size = size
-        self.slots = [None] * self.size
-        self.value = [None] * self.size
+        self.keys = [None] * self.size
+        self.values = [None] * self.size
          
     def put(self,key,value):
         hashValue = self.hashFunction(key, self.size)
  
-        if self.slots[hashValue] == None:
-            self.slots[hashValue] = key
-            self.value[hashValue] = value
+        if self.keys[hashValue] == None:
+            self.keys[hashValue] = key
+            self.values[hashValue] = value
         else:
-            if self.slots[hashValue] == key:
-                self.value[hashValue] = value  
+            if self.keys[hashValue] == key:
+                self.values[hashValue] = value  
             else:
                 nextSlot = self.rehash(hashValue, self.size)
 
-                while self.slots[nextSlot] != None and self.slots[nextSlot] != key:
+                while self.keys[nextSlot] != None and self.keys[nextSlot] != key:
                     nextSlot = self.rehash(nextSlot,self.size)
 
-                if self.slots[nextSlot] == None:
-                    self.slots[nextSlot]=key
-                    self.value[nextSlot]=value
+                if self.keys[nextSlot] == None:
+                    self.keys[nextSlot] = key
+                    self.values[nextSlot] = value
                 else:
-                    self.value[nextSlot] = value 
+                    self.values[nextSlot] = value 
+
+    def delete(self,key):
+        hashValue = self.hashFunction(key, self.size)
+
+        if self.get(key) != None:
+            if self.keys[hashValue] == key:
+                self.keys[hashValue] = None
+                self.values[hashValue] = None
+            else:
+                nextSlot = self.rehash(hashValue, self.size)
+                while self.keys[nextSlot] != key:
+                    nextSlot = self.rehash(nextSlot,self.size)
+
+                self.keys[hashValue] = None
+                self.values[hashValue] = None
+
+        
     
     def get(self,key):
 
@@ -38,19 +54,15 @@ class HashTable(object):
         isFound = False
         pos = startSlot
          
-        while self.slots[pos] != None and not isFound and not shouldStop:
+        while self.keys[pos] != None and not isFound and not shouldStop:
              
-            if self.slots[pos] == key:
+            if self.keys[pos] == key:
                 isFound = True
-                value = self.value[pos]
+                value = self.values[pos]
             else:
                 pos=self.rehash(pos,self.size)
                 if pos == startSlot:
                     shouldStop = True
-
-        if(value == None):
-            return "Could not find any value with this key"
-
 
         return value
  
@@ -60,14 +72,15 @@ class HashTable(object):
     def rehash(self,oldHash,size):
         return (oldHash+1) % size
     
-test = HashTable(6)
+myTable = HashTable(6)
 
 keys = [10, 20, 40, 44, 53, 22]
 values = [1, 122, 34, 44, 550, 2]
 
 for x in range(len(keys)):
-    test.put(keys[x], values[x])
+    myTable.put(keys[x], values[x])
 
-print(test.slots)
-print(test.value)
-print(test.get(40))
+myTable.get(40)
+myTable.delete(40)
+print(myTable.keys)
+print(myTable.values)
